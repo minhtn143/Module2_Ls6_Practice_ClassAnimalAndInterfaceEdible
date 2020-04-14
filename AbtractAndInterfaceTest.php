a80d6fa8672fd6ba42978d86204c06a374421ea3
minhtn143 <minhtn143@gmail.com>
2020-04-14 09:24:49 +0700

    upload

---
+++ AbtractAndInterfaceTest.php
@@ -0,0 +1,28 @@
+<?php
+include('AbstractClass/Chicken.php');
+include('AbstractClass/Tiger.php');
+include('Fruits/Apple.php');
+include('Fruits/Orange.php');
+
+
+
+echo ('---- Animals<br>');
+$animals[0] = new Tiger();
+$animals[1] = new Chicken();
+
+foreach ($animals as $animal) {
+    echo $animal->makeSound(). ' ';
+    if ($animal instanceof Chicken) {
+        echo $animal->howToEat(). ' ';
+    }
+}
+
+
+echo ('---- Fruits<br>');
+
+$fruits[0] = new Apple();
+$fruits[1] = new Orange();
+
+foreach ($fruits as $fruit) {
+    echo $fruit->howToEat() . '<br>';
+}
